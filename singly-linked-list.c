8ecb13c2d3a96fc3a922b9817439ca7d49eb7ba5
이 연규 <81019562+Lee-yeonkyu@users.noreply.github.com>
2021-04-12 19:39:38 +0900

    Implement and modify the rest of the functions

--- singly-linked-list.c
+++ singly-linked-list.c
@@ -162,21 +162,33 @@ int insertNode(headNode* h, int key) {
 
 	listNode *temp=(listNode *)malloc(sizeof(listNode));
 	listNode *New=(listNode *)malloc(sizeof(listNode));
-	listNode *Pre;
+	listNode *pre=(listNode *)malloc(sizeof(listNode));
 	New->key=key;
 	New->link=NULL;
+	pre->link=NULL;
 	temp=h->first; //temp을 first가 가르키는 값으로 지정한다.
 
 	if(h->first ==NULL){ // 현재 리스트가 공백인 경우에.
 		h->first =New; //first가 New를 가르킬수 있게 만든다.
-		return New;
+		return 0;
 	}
-	while(temp->key>key) //입력받은 키값보다 리스트안의 키값이 클때까지 반복한다.
+	while(temp->key<key){//입력받은 키값보다 리스트안의 키값이 클때까지 반복한다.
+		if(temp->link==NULL){
+			temp->link=New;//모든 수보다 입력한 키값이 크다면 마지막에 넣어준다.
+			return 0;	}
+		pre=temp;
 		temp=temp->link; //temp노드가 가르키는 곳을 temp로 바꾼다.
-	New->link=temp->link; //temp노드가 가르키는 곳을 new도 가르키게한다.
-	temp->link=New;//temp노드가 new를 가르키도록 지정한다.
+	}
+	if(pre->link==NULL) //첫번째 값보다 작은경우.
+	{
+		New->link = h->first; //first가 가르키고 있는곳을 New가 가르킨다.
+		h->first = New; // first가 New를 가르키게 만든다
+		return 0;
+	}
+	New->link=pre->link; //temp노드가 가르키는 곳을 new도 가르키게한다.
+	pre->link=New;//temp노드가 new를 가르키도록 지정한다.
 
-	return New;
+	return 0;
 }
 
 /**
@@ -186,18 +198,19 @@ int insertLast(headNode* h, int key) {
 
 	listNode* last;
 		listNode* New =(listNode*)malloc(sizeof(listNode)); //새로운 노드를 할당해준다.
-		New->key=key; //노드안에 key값을 넣어준다.
 		New->link=NULL; //링크 필드는 아직 받지 않았기에 공백 할당.
 
-			if(h->first ==NULL){ // 현재 리스트가 공백인 경우에.
-				h->first =New; //first가 New를 가르킬수 있게 만든다.
-				return New;
+			if(h !=NULL){ // 현재 리스트가 공백인 경우에.
+				New->link = h->first; //first가 가르키고 있는곳을 new가 가르킨다.
+					h->first = New; // first가 new를 가르키게 만든다.
+				return 1;
 			}
 			last=h->first; //first가 가리키는 값을 last로 놓는다.
-			while(last->link !=NULL) //last의 링크가 가르키는 값이 없을때까지 반복해 마지막 노드를 찾는다.
+			while(last->link !=NULL){//last의 링크가 가르키는 값이 없을때까지 반복해 마지막 노드를 찾는다.
 				last=last->link;//last노드가 가르키는 곳을 last로 바꾼다.
+			}
 			last->link=New; //last가 New를 가르키도록 지정한다.
-			return New;
+			return 1;
 }
 
 
@@ -205,9 +218,14 @@ int insertLast(headNode* h, int key) {
  * list의 첫번째 노드 삭제
  */
 int deleteFirst(headNode* h) {
+	listNode *del;
+	if(h->first==NULL)return 0; // 공백일시 함수 나간다.
+	del=h->first; //first가 del을 가르키게한다.
 
+	h->first=h->first->link; //first가 first의 링크값을 가르키도록 지정.
+	free(del); //del 할당해제
 
-	return 0;
+	return 1;
 }
 
 
@@ -215,6 +233,22 @@ int deleteFirst(headNode* h) {
  * list에서 key에 대한 노드 삭제
  */
 int deleteNode(headNode* h, int key) {
+
+	listNode *find =(listNode*)malloc(sizeof(listNode));
+	listNode *pre =(listNode*)malloc(sizeof(listNode));
+	listNode *del;
+	pre->link=NULL;
+	find=h->first; // first가 가르키는 값을 find로 놓는다.
+
+	while(find->key !=key){//find의 키값과 같은 값이 나올때까지 반복해 원하는 노드를 찾는다.
+		pre=find;//find값을 pre에 넣어준다.
+		if(find->link==NULL) return 0; //모든 값이 다른경우 함수에서 나간다.
+		del=find->link;//find가 가르키는 곳을 del이 가르키게 한다.
+		find=find->link;//find노드가 가르키는 곳을 find로 바꾼다.
+	}
+	if(pre->link == NULL) h->first=h->first->link; // 첫번째 키가 같다면 첫번째 노드를 없애준다.
+	pre->link=find->link; //find가 가르키고있는 값을 pre가 가르킨다.
+	free(del);
 
 	return 0;
 
@@ -224,6 +258,25 @@ int deleteNode(headNode* h, int key) {
  * list의 마지막 노드 삭제
  */
 int deleteLast(headNode* h) {
+	listNode *del;
+	listNode *pre;
+
+	pre=h->first; // 삭제할 노드의 앞의 노드를 첫번째로 지정.
+	del=h->first->link; //삭제할 노드를 두번째로 지정.
+	if(h->first==NULL) return -1; //빈노드면 종료.
+	if(h->first->link==NULL) { //노드가 한개라면
+		free(h->first); //그 노드의 할당 해제.
+		h->first=NULL;
+		return 0;
+	}
+	else{
+		while(del->link !=NULL){//노드를 끝까지 반복한다.
+			pre=del; //pre에 del값을 넣어준다.
+			del=del->link; // del의 링크가 가르키는 값으로 del을 바꿔준다.
+		}
+		free(del); //del을 할당 해제해준다.
+		pre->link=NULL;
+	}
 
 	return 0;
 }
@@ -234,6 +287,19 @@ int deleteLast(headNode* h) {
  */
 int invertList(headNode* h) {
 
+	listNode *p,*q,*r;
+
+	p=h->first;
+	q=NULL;
+	while(p!=NULL)
+	{
+		r=q;
+
+		q=p;
+		p=p->link;
+		q->link=r;
+	}
+	h->first=q;
 	return 0;
 }
 
